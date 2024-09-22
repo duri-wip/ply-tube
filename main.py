ad160efc098ff6c97cbd421a2293779272e2b173
seowoo <143095151+duri-wip@users.noreply.github.com>
2024-09-22 20:04:58 +0900

    Update main.py

--- main.py
+++ main.py
@@ -14,18 +14,18 @@ mlflow.set_tracking_uri(os.getenv('MLFLOW_TRACKING_URI'))
 print("-----------tracking uri 설정-----------")
 print(os.getenv('MLFLOW_TRACKING_URI'))
 
-# #전처리
-# from src.Preprocessing import preprocessing
-# from src.Dataset import LSTMdataset
+#전처리
+from src.Preprocessing import preprocessing
+from src.Dataset import LSTMdataset
 
-# playlists = preprocessing(playlists)
+playlists = preprocessing(playlists)
 
-# dataset = LSTMdataset(playlists = playlists)
+dataset = LSTMdataset(playlists = playlists)
 
 
-# #모델 학습
-# from src.Train import run_training
-# run_training(dataset=dataset)
+#모델 학습
+from src.Train import run_training
+run_training(dataset=dataset)
 
 # 운영중인 모델과 비교해서 새로운 모델로 변경
 from utils.ProductionAlias import production_alias
